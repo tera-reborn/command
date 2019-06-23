4ee5fc3ba94b949785ff4252ee68ed5f30746f1f
Caali <modsfortera@gmail.com>
2019-06-23 14:49:06 +0200

    Refactoring. Fix race condition.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict'
 
 const PRIVATE_CHANNEL_INDEX = 7,
-      PRIVATE_CHANNEL_ID = -2 >>> 0,
-      PUBLIC_MATCH = /^!([^!].*)$/
+    PRIVATE_CHANNEL_ID = -2 >>> 0,
+    PUBLIC_MATCH = /^!([^!].*)$/;
 
 
 class CommandBase {
     constructor(mod) {
         this.mod = mod;
         this.loaded = false;
+        this.queue = [];
         this.hooks = {};
 
-        mod.hook('S_LOGIN', 'raw', () => { this.loaded = false });
+        mod.hook('S_LOGIN', 'raw', () => { this.loaded = false; });
 
         mod.hook('S_LOAD_CLIENT_USER_SETTING', 'raw', () => {
-            if(!this.loaded) {
+            if (!this.loaded) {
                 this.loaded = true;
                 process.nextTick(() => {
                     mod.send('S_JOIN_PRIVATE_CHANNEL', 2, {
@@ -26,16 +27,23 @@ class CommandBase {
 
                     if (this.mod.settings.login_message)
                         this.message(null, `Client ${this.mod.majorPatchVersion}.${this.mod.minorPatchVersion} - Protocol ${this.mod.protocolVersion}`);
+
+                    mod.setTimeout(() => {
+                        if (this.queue) {
+                            this.queue.forEach(entry => this.message(...entry));
+                            this.queue = null;
+                        }
+                    }, 2000);
                 });
             }
         })
 
-        mod.hook('S_JOIN_PRIVATE_CHANNEL', 1, event => event.index == PRIVATE_CHANNEL_INDEX ? false : undefined);
-        mod.hook('C_LEAVE_PRIVATE_CHANNEL', 1, event => event.index == PRIVATE_CHANNEL_INDEX ? false : undefined);
+        mod.hook('S_JOIN_PRIVATE_CHANNEL', 1, event => event.index === PRIVATE_CHANNEL_INDEX ? false : undefined);
+        mod.hook('C_LEAVE_PRIVATE_CHANNEL', 1, event => event.index === PRIVATE_CHANNEL_INDEX ? false : undefined);
 
         if (mod.platform !== 'classic') {
             mod.hook('C_REQUEST_PRIVATE_CHANNEL_INFO', 1, event => {
-                if(event.channelId === PRIVATE_CHANNEL_ID) {
+                if (event.channelId === PRIVATE_CHANNEL_ID) {
                     mod.send('S_REQUEST_PRIVATE_CHANNEL_INFO', 1, {
                         owner: 1,
                         password: 0,
@@ -49,69 +57,69 @@ class CommandBase {
 
         let lastError,
             hookCommand = message => {
-                let args = null
+                let args = null;
 
                 try {
-                    args = parseArgs(stripOuterHTML(message))
-                } catch(e) {
-                    return 'Syntax error: ' + e.message
+                    args = parseArgs(stripOuterHTML(message));
+                } catch (e) {
+                    return 'Syntax error: ' + e.message;
                 }
 
                 try {
-                    if(!this.exec(args))
-                        return 'Unknown command "' + args[0] + '".'
-                } catch(e) {
-                    this.message(null, 'Error running callback for command "' + args[0] + '".')
-                    mod.error(e)
+                    if (!this.exec(args))
+                        return 'Unknown command "' + args[0] + '".';
+                } catch (e) {
+                    this.message(null, 'Error running callback for command "' + args[0] + '".');
+                    mod.error(e);
                 }
             }
 
-        mod.hook('C_CHAT', 1, {order: -10}, event => {
-            if(event.channel === 11 + PRIVATE_CHANNEL_INDEX) {
+        mod.hook('C_CHAT', 1, { order: -10 }, event => {
+            if (event.channel === 11 + PRIVATE_CHANNEL_INDEX) {
                 lastError = hookCommand(event.message);
-                if(!lastError)
+                if (!lastError)
                     return false;
-            } else if(this.mod.settings.public_enable) {
+            } else if (this.mod.settings.public_enable) {
                 const str = PUBLIC_MATCH.exec(stripOuterHTML(event.message));
 
-                if(str) {
+                if (str) {
                     lastError = hookCommand(str[1]);
-                    if(!lastError)
+                    if (!lastError)
                         return false;
                 }
             }
         });
 
         // Let other modules handle possible commands before we silence them
-        mod.hook('C_CHAT', 1, {order: 10, filter: {silenced: null}}, event => {
-            if(lastError) {
-                if(!event.$silenced)
+        mod.hook('C_CHAT', 1, { order: 10, filter: { silenced: null } }, event => {
+            if (lastError) {
+                if (!event.$silenced)
                     this.message(null, lastError);
                 lastError = undefined;
                 return false;
             }
         });
 
-        mod.hook('C_WHISPER', 1, {order: -10}, event => {
-            if(!this.mod.settings.public_enable)
+        mod.hook('C_WHISPER', 1, { order: -10 }, event => {
+            if (!this.mod.settings.public_enable)
                 return;
 
             const str = PUBLIC_MATCH.exec(stripOuterHTML(event.message));
 
-            if(str) {
+            if (str) {
                 lastError = hookCommand(str[1]);
-                if(!lastError)
+                if (!lastError)
                     return false;
             }
         });
 
         // Let other modules handle possible commands before we silence them
-        mod.hook('C_WHISPER', 1, {order: 10, filter: {silenced: null}}, event => {
-            if(!this.mod.settings.public_enable)
+        mod.hook('C_WHISPER', 1, { order: 10, filter: { silenced: null } }, event => {
+            if (!this.mod.settings.public_enable)
                 return;
 
-            if(lastError) {
-                if(!event.$silenced)
+            if (lastError) {
+                if (!event.$silenced)
                     this.message(null, lastError);
                 lastError = undefined;
                 return false;
@@ -121,9 +129,12 @@ class CommandBase {
         // Add own commands
         this.add('proxy', {
             $default() {
-                this.message(null, `Proxy commands:`)
-                this.message(null, `onlychannel - Toggles ability to enter commands in proxy channel only (recommended) or all channels`)
-                this.message(null, `loginmessage - Toggles proxy status message shown on login`)
+                this.message(null, `Proxy commands:`);
+                this.message(null, `onlychannel - Toggles ability to enter commands in proxy channel only (recommended) or all channels`);
+                this.message(null, `loginmessage - Toggles proxy status message shown on login`);
+                this.message(null, `load [module name] - Loads the given module`);
+                this.message(null, `unload [module name] - Unloads the given module`);
+                this.message(null, `reload [module name] - Reloads the given module`);
             },
             onlychannel() {
                 this.mod.settings.public_enable = !this.mod.settings.public_enable;
@@ -134,37 +145,37 @@ class CommandBase {
                 this.message(null, `Proxy login message ${this.mod.settings.login_message ? 'enabled' : 'disabled'}`);
             },
             load(name) {
-                if(!name) {
+                if (!name) {
                     this.message(null, 'No module name specified!');
                     return;
                 }
 
                 const result = this.mod.manager.load(name);
-                if(result)
+                if (result)
                     this.message(null, `Loaded "${name}"!`);
                 else
                     this.message(null, `Unable to load "${name}", check log for details!`);
             },
             unload(name) {
-                if(!name) {
+                if (!name) {
                     this.message(null, 'No module name specified!');
                     return;
                 }
 
                 const result = this.mod.manager.unload(name);
-                if(result)
+                if (result)
                     this.message(null, `Unloaded "${name}"!`);
                 else
                     this.message(null, `Unable to unload "${name}", check log for details!`);
             },
             reload(name) {
-                if(!name) {
+                if (!name) {
                     this.message(null, 'No module name specified!');
                     return;
                 }
 
                 const result = this.mod.manager.reload(name);
-                if(result)
+                if (result)
                     this.message(null, `Reloaded "${name}"!`);
                 else
                     this.message(null, `Unable to reload "${name}", check log for details!`);
@@ -174,12 +185,12 @@ class CommandBase {
 
     exec(str) {
         const args = Array.isArray(str) ? str : parseArgs(str);
-        if(args.length === 0)
+        if (args.length === 0)
             return false;
 
         const cb = this.hooks[args[0].toLowerCase()];
 
-        if(cb) {
+        if (cb) {
             cb.call(...args);
             return true;
         }
@@ -188,69 +199,74 @@ class CommandBase {
     }
 
     add(cmd, cb, ctx) {
-        if(typeof cb === 'function') {
-            if(ctx !== undefined)
-                cb = cb.bind(ctx)
-        } else if(typeof cb === 'object') {
+        if (typeof cb === 'function') {
+            if (ctx !== undefined)
+                cb = cb.bind(ctx);
+        } else if (typeof cb === 'object') {
             cb = makeSubCommandHandler(cb, ctx);
         } else {
             throw new Error('Callback must be a function or object');
         }
 
-        if(Array.isArray(cmd)) {
-            for(let c of cmd)
+        if (Array.isArray(cmd)) {
+            for (let c of cmd)
                 this.add(c, cb);
             return;
         }
 
-        if(typeof cmd !== 'string')
+        if (typeof cmd !== 'string')
             throw new Error('Command must be a string or array of strings');
-        if(cmd === '')
+        if (cmd === '')
             throw new Error('Command must not be an empty string');
 
         cmd = cmd.toLowerCase();
-        if(this.hooks[cmd])
+        if (this.hooks[cmd])
             throw new Error('Command already registered:', cmd);
 
         this.hooks[cmd] = cb;
     }
 
     remove(cmd) {
-        if(Array.isArray(cmd)) {
-            for(let c of cmd)
+        if (Array.isArray(cmd)) {
+            for (let c of cmd)
                 this.remove(c);
             return;
         }
 
-        if(typeof cmd !== 'string')
+        if (typeof cmd !== 'string')
             throw new Error('Command must be a string or array of strings');
-        if(cmd === '')
+        if (cmd === '')
             throw new Error('Command must not be an empty string');
 
         delete this.hooks[cmd.toLowerCase()];
     }
 
     message(modName, msg) {
-        this.mod.send('S_PRIVATE_CHAT', 1, {
-            channel: PRIVATE_CHANNEL_ID,
-            authorID: 0,
-            authorName: '',
-            message: (modName && !this.mod.settings.hide_module_names) ? `[${modName}] ${msg}` : ` ${msg}`,
-        });
+        if (this.queue) {
+            // Not ready yet, delay sending the message
+            this.queue.push([modName, msg]);
+        } else {
+            this.mod.send('S_PRIVATE_CHAT', 1, {
+                channel: PRIVATE_CHANNEL_ID,
+                authorID: 0,
+                authorName: '',
+                message: (modName && !this.mod.settings.hide_module_names) ? `[${modName}] ${msg}` : ` ${msg}`,
+            });
+        }
     }
 }
 
 function makeSubCommandHandler(_obj, ctx) {
     const obj = {};
 
-    for(let cmd in _obj) {
+    for (let cmd in _obj) {
         const cb = _obj[cmd];
 
         cmd = cmd.toLowerCase();
 
-        if(typeof cb === 'function')
+        if (typeof cb === 'function')
             obj[cmd] = ctx !== undefined ? cb.bind(ctx) : cb;
-        else if(typeof cb === 'object')
+        else if (typeof cb === 'object')
             obj[cmd] = makeSubCommandHandler(cb, ctx);
         else
             throw new Error('Sub-command callback must be a function or object');
@@ -259,7 +275,7 @@ function makeSubCommandHandler(_obj, ctx) {
     return function subCommandHandler(cmd) {
         const cb = (cmd !== undefined ? obj[cmd.toLowerCase()] : obj.$none) || obj.$default;
 
-        if(cb)
+        if (cb)
             cb.apply(null, (arguments && cb !== obj.$default) ? Array.prototype.slice.call(arguments, 1) : arguments);
     }
 }
@@ -275,16 +291,16 @@ function parseArgs(str) {
     let arg = '',
         quote = '';
 
-    for(let i = 0, c = ''; i < str.length; i++) {
+    for (let i = 0, c = ''; i < str.length; i++) {
         c = str[i];
 
-        switch(c) {
+        switch (c) {
             case '<':
                 parseHTML.lastIndex = i + 1;
 
                 let len = parseHTML.exec(str);
 
-                if(!len)
+                if (!len)
                     throw new Error('HTML parsing failure');
 
                 len = len[0].length;
@@ -294,26 +310,26 @@ function parseArgs(str) {
             case '\\':
                 c = str[++i];
 
-                if(c === undefined)
+                if (c === undefined)
                     throw new Error('Unexpected end of line');
 
                 arg += c;
                 break;
             case '\'':
             case '"':
-                if(arg === '' && quote === '') {
+                if (arg === '' && quote === '') {
                     quote = c;
                     break;
                 }
-                if(quote === c) {
+                if (quote === c) {
                     quote = '';
                     break;
                 }
                 arg += c;
                 break
             case ' ':
-                if(quote === '') {
-                    if(arg !== '') {
+                if (quote === '') {
+                    if (arg !== '') {
                         args.push(arg);
                         arg = '';
                     }
@@ -324,8 +340,8 @@ function parseArgs(str) {
         }
     }
 
-    if(arg !== '') {
-        if(quote !== '')
+    if (arg !== '') {
+        if (quote !== '')
             throw new Error('Expected ' + quote);
 
         args.push(arg);
